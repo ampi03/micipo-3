3aed3e8aca81be93652eb6bea56d0b43c2ba2aa7
proyectopp <proyectopp990@gmail.com>
2022-11-23 00:52:18 -0300

    slider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'navbar',
+    redirectTo: 'slider',
     pathMatch: 'full'
   },
   {
@@ -19,6 +19,11 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./paginas/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'slider',
+    loadChildren: () => import('./paginas/slider/slider.module').then( m => m.SliderPageModule)
+  },
+
 
 
 ];
